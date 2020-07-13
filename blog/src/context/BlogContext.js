f70787f8f12a3af42f504816e35b9ae0cb74e29d
Jacob Miller <millerjacob@seattleu.edu>
2020-07-13 13:48:22 -0700

    Added edit screen callback navigation

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,27 +1,48 @@
 import createDataContext from "./createDataContext";
 
-const blogReducer = (state,action) => {
+const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
-            return[...state,{id:Math.floor(Math.random() * 99999), title:action.payload.title, content: action.payload.content}];
+            return [...state, {
+                id: Math.floor(Math.random() * 99999),
+                title: action.payload.title,
+                content: action.payload.content
+            }];
         case 'delete_blogpost':
-            return state.filter((blogPost)=> blogPost.id !== action.payload);
+            return state.filter((blogPost) => blogPost.id !== action.payload);
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            });
         default:
             return state;
     }
 };
 
-const addBlogPost = dispatch =>{
-    return(title,content, callback) => {
-        dispatch({type: 'add_blogpost', payload:{title,content}});
-        callback();
+const addBlogPost = dispatch => {
+    return (title, content, callback) => {
+        dispatch({type: 'add_blogpost', payload: {title, content}});
+        if(callback)
+            callback();
     };
 };
 
-const deleteBlogPost = dispatch =>{
-    return (id) =>{
+const deleteBlogPost = dispatch => {
+    return (id) => {
         dispatch({type: 'delete_blogpost', payload: id})
-    }
-}
+    };
+};
 
-export const {Context, Provider} = createDataContext(blogReducer, {addBlogPost, deleteBlogPost}, [{title:'TEST POST', content: 'TEST CONTENT', id:1}]);
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({type: 'edit_blogpost', payload: {id, title, content}})
+        if(callback)
+            callback();
+    };
+};
+
+export const {Context, Provider} = createDataContext(blogReducer, {
+    addBlogPost,
+    deleteBlogPost,
+    editBlogPost
+}, [{title: 'TEST POST', content: 'TEST CONTENT', id: 1}]);
