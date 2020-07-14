4be5c20177948d23e0ae4e6714b25491638fe6a3
Jacob Miller <millerjacob@seattleu.edu>
2020-07-14 11:42:35 -0700

    Establishing mongo connection

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 const express = require('express');
-
 const db_string = process.env.CLOUD_STRING;
+const mongoose = require('mongoose');
 
 const app = express();
 
 const mongoUri = db_string;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+});
 
-console.log(db_string);
+mongoose.connection.on('error', (err) => {
+    console.log('Connection error' + err);
+});
 
-app.get('/', (req, res) =>{
-   res.send('Hi there!');
+app.get('/', (req, res) => {
+    res.send('Hi there!');
 });
 
 app.listen(3000, () => {
