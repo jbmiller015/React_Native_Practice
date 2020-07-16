adf88c51103a846f168969c0a0679fde9333854a
Jacob Miller <millerjacob@seattleu.edu>
2020-07-16 10:58:58 -0700

    Implemented Authorization middleware using JWT

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -5,14 +5,14 @@ const db_string = process.env.CLOUD_STRING;
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const bodyParser = require('body-parser');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
 
-const mongoUri = db_string;
-mongoose.connect(mongoUri, {
+mongoose.connect(db_string, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -25,8 +25,8 @@ mongoose.connection.on('error', (err) => {
     console.log('Connection error' + err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth,(req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
+const jwtString = process.env.JWT_AUTH;
 
 const router = express.Router();
 
@@ -10,7 +13,9 @@ router.post('/signup', async (req, res) => {
     try {
         const user = new User({email, password});
         await user.save();
-        res.send('You made a post request');
+
+        const token = jwt.sign({userId: user._id},jwtString);
+        res.send({token});
     }catch (e) {
         res.status(422).send(e.message);
     }
