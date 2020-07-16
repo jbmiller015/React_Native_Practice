5e3e6727a93fe251883e380ca83d562d92ec379b
Jacob Miller <millerjacob@seattleu.edu>
2020-07-16 09:59:09 -0700

    Updated user model. Added try/catch to save

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const user = mongoose.model('User');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
     console.log(req.body);
     const {email, password} = req.body;
-    const user = new user({email,password});
-    await user.save();
-    res.send('You made a post request');
+    try {
+        const user = new User({email, password});
+        await user.save();
+        res.send('You made a post request');
+    }catch (e) {
+        res.status(422).send(e.message);
+    }
+
+    
 });
 
 module.exports = router;
