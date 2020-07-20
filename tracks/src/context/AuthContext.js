dc583fd31dadc66aff7f88c7caf06b918e03f70c
jbmiller015 <jbmiller015@gmail.com>
2020-07-20 15:17:34 -0700

    adding singup case

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -6,23 +6,25 @@ const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return {...state, errorMessage: action.payload};
+        case 'signup':
+            return {errorMessage: '',token:action.payload};
         default:
             return state;
     }
 };
 
 //Scaffolding for functions
-const signup = (dispatch) => {
-    return async ({email, password}) => {
-        try {
-            const response = await trackerApi.post('/signup', {email, password});
-            await AsyncStorage.setItem('token', response.data.token);
-            dispatch({type:'signup', payload: response.data.token})
-        } catch (e) {
-            dispatch({type: 'add_error', payload: 'Something went wrong w/ signup'})
-        }
-    };
+const signup = (dispatch) => async ({email, password}) => {
+    try {
+        const response = await trackerApi.post('/signup', {email, password});
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({type: 'signup', payload: response.data.token})
+
+    } catch (e) {
+        dispatch({type: 'add_error', payload: 'Something went wrong w/ signup'})
+    }
 };
+
 
 const signin = (dispatch) => {
     return ({email, password}) => {
